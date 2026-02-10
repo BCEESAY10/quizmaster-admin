5961cb881d86e6af43d2fb932d0a239bb5e56e4f
BCEESAY10 <bamfaceesay30@gmail.com>
2026-02-10 16:09:22 +0000

    refactor: remove the notification bell

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Menu, Bell, Search } from "lucide-react";
+import { Menu, Search } from "lucide-react";
 import { Input } from "@/src/components/ui/Input";
 import { Session } from "next-auth";
 import { Avatar } from "../ui/Avatar";
@@ -34,12 +34,6 @@ export function Header({ onMenuClick, session }: HeaderProps) {
 
         {/* Right section */}
         <div className="flex items-center space-x-4">
-          {/* Notifications */}
-          <button className="relative p-2 text-gray-600 hover:bg-gray-100 rounded-lg">
-            <Bell className="h-6 w-6" />
-            <span className="absolute top-1 right-1 h-2 w-2 bg-red-500 rounded-full" />
-          </button>
-
           {/* User menu */}
           <div className="flex items-center space-x-3">
             <div className="hidden md:block text-right">
